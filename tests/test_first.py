44d4ed0cd86b606bf20b65b7683041e330e948ac
Pooja Tiwari <pooja.tiwari@contractoralefeducation.com>
2024-04-10 19:09:08 +0530

    Added some text 9

--- tests/test_first.py
+++ tests/test_first.py
@@ -5,30 +5,31 @@ logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def test_one():
     logger.info(f"\Test 1...")
-    assert 1==1
+    assert 1 == 1
     print(f"\Test 1 ====")
 
 
 def test_two():
     logger.info(f"\Test 2...")
-    assert 1==1
+    assert 1 == 1
     print(f"\Test 2 ====")
 
 
 def test_three():
     logger.info(f"\Test 3...")
-    assert 1==1
+    assert 1 == 1
     print(f"\Test 3 ====")
 
 
-def test_four():
-    logger.info(f"\Test 4...")
-    assert 4==1
-    print(f"\Test 4 ====")
+# def test_four():
+#     logger.info(f"\Test 4...")
+#     assert 4==1
+#     print(f"\Test 4 ====")
 
-def test_five():
-    logger.info(f"\Test 5...")
-    assert 5==1
-    print(f"\Test 5 ====")
+# def test_five():
+#     logger.info(f"\Test 5...")
+#     assert 5==1
+#     print(f"\Test 5 ====")
